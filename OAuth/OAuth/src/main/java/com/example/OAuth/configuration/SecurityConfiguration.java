110e2912ed0847762e3a49e03740174b4f45e452
Logavarshhni23 <logavarshhnis2007@gmail.com>
2026-06-08 12:42:59 +0530

    Day-10

--- OAuth/OAuth/src/main/java/com/example/OAuth/configuration/SecurityConfiguration.java
+++ OAuth/OAuth/src/main/java/com/example/OAuth/configuration/SecurityConfiguration.java
@@ -1,4 +1,21 @@
 package com.example.OAuth.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+        //user can enter without authorization due to permit all
+        http.authorizeHttpRequests(requests-> requests.requestMatchers("/public").permitAll());
+        //only user with authentication is allowed
+        http.authorizeHttpRequests((requests->requests.anyRequest().authenticated()));
+        http.oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
 }
--- OAuth/OAuth/src/main/java/com/example/OAuth/controller/HelloController.java
+++ OAuth/OAuth/src/main/java/com/example/OAuth/controller/HelloController.java
@@ -1,4 +1,18 @@
 package com.example.OAuth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    //can access without authentication
+    @GetMapping("/public")
+    public String login(){
+        return "Authenticated";
+    }
+    //only authorized user can access
+    @GetMapping("/private")
+    public String log(){
+        return "Authorization";
+    }
 }
